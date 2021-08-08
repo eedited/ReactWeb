645dc2dac50c37946d4b4867bfc239dbdfaa2be6
airplane9876 <tjdnf2eoeld@naver.com>
2021-08-08 19:29:08 +0900

    Design and Responsive web is applied to LargeVideo in video play pages

---
+++ src/containers/Video/VideoHeader.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { selectorStateType, useAppSelector } from '../../hooks';
+import { videoAPISuccessReturnProp } from '../../lib/api/video';
+import LargeVideoHeader from '../../components/Video/LargeVideoHeader';
+
+interface fromReducerType{
+    Video: videoAPISuccessReturnProp|null
+}
+
+const LargeVideoHeaderContainer: React.FC = () => {
+    const {
+        Video,
+    }: fromReducerType = useAppSelector((state: selectorStateType) => ({
+        Video: state.videoReducer.video,
+    }));
+    if (Video === null) {
+        return <div />;
+    }
+    return (
+        <LargeVideoHeader video={Video.video} />
+    );
+};
+
+export default LargeVideoHeaderContainer;
--- src/pages/VideoPage.tsx
+++ src/pages/VideoPage.tsx
@@ -3,8 +3,10 @@ import qs from 'qs';
 import { Redirect, RouteComponentProps } from 'react-router';
 import VideoInfoContainer from '../containers/Video/LargeVideoContainer';
 import BaseTemplate from './BaseTemplate';
+import LargeVideoDescription from '../containers/Video/LargeVideoDescriptionContainer';
 import MoreByUser from '../containers/Video/MoreByUserContainer';
 import UserMabyLike from '../containers/Video/UserMaybeLike';
+import VideoHeader from '../containers/Video/VideoHeader';
 
 type props = RouteComponentProps
 
@@ -15,6 +17,7 @@ const VideoPage: React.FC<props> = ({ location }: props) => {
     const { videoId }: qs.ParsedQs = query;
     return (
         <BaseTemplate>
+            <VideoHeader />
             <>
                 {
                     typeof (videoId) === 'string'
@@ -27,11 +30,12 @@ const VideoPage: React.FC<props> = ({ location }: props) => {
                         )
                 }
             </>
-            <section>
+            <LargeVideoDescription />
+            {/* <section>
                 discription
 
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque blanditiis, animi molestias fuga corrupti sint laborum eum tempora incidunt autem quasi sit dolorum amet nam minus veniam modi fugiat deleniti quos quam sapiente, quaerat inventore necessitatibus? Eveniet nihil doloremque minima maxime ducimus esse, illo in sit ullam placeat! Est, quisquam!
-            </section>
+            </section> */}
             <hr />
             <MoreByUser />
             <hr />
