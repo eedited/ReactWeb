c0f5602e4b894c36884cc2fcc9ad4e17fc6cfc02
JeonMinSu <minsu2530@u.sogang.ac.kr>
2021-08-09 16:59:22 +0900

    [ADD]ScrollTopElement to go top when clicked Link element

---
+++ src/components/common/ScrollTop.tsx
@@ -0,0 +1,23 @@
+import React, { useEffect } from 'react';
+import { RouteComponentProps, withRouter } from 'react-router-dom';
+
+interface props{
+    history: RouteComponentProps['history'],
+    children?: React.ReactNode
+}
+const ScrollTop: React.FC<props> = ({ history, children }: props) => {
+    useEffect(() => {
+        const unlisten: ()=> void = history.listen(() => {
+            window.scrollTo(0, 0);
+        });
+        return () => {
+            unlisten();
+        };
+    }, [history]);
+    return <>{children}</>;
+};
+ScrollTop.defaultProps = {
+    children: '',
+};
+
+export default withRouter(ScrollTop);
