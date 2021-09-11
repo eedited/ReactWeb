00d8065e95d1017c2b27b470ad3085c63adbfc13
QuqqU <rldnd913@naver.com>
2021-09-11 14:14:31 +0900

    re-type & add pre-header RDX to redux module

--- src/@types/filter.d.ts
+++ src/@types/filter.d.ts
@@ -1,22 +1,26 @@
 import { CaseReducerActions, PayloadAction } from '@reduxjs/toolkit';
 import { WritableDraft } from 'immer/dist/internal';
 
-export declare global{
-    namespace filterModule{
-        export type filterKey = 'category' | 'platform' | 'program' | 'sorting';
-        interface StateType{
-            [key: string]: string | null;
-            category: null,
-            platform: null,
-            program: null,
-            sorting: 'popular',
-        }
-        type ActionType =CaseReducerActions<{
-            setFilter(state: WritableDraft<StateType>, action: PayloadAction<{
-                type: string;
-                set: string;
-            }>): void;
-        }>
+export declare global {
+    namespace RDXFilterModule {
+        export type FilterKey = 'category' | 'platform' | 'program' | 'sorting';
 
+        interface StateType {
+            [key: string]: string | null;
+            category: null;
+            platform: null;
+            program: null;
+            sorting: 'popular';
+        }
+
+        type ActionType = CaseReducerActions<{
+            setFilter(
+                state: WritableDraft<StateType>,
+                action: PayloadAction<{
+                    type: string;
+                    set: string;
+                }>,
+            ): void;
+        }>;
     }
 }
