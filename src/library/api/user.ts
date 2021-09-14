fd2e2046a31bb6872e03f75bbfb556a1274ac7d4
QuqqU <rldnd913@naver.com>
2021-09-14 18:43:16 +0900

    refac types to uppercase

--- src/library/api/user.ts
+++
@@ -1,7 +0,0 @@
-import { AxiosResponse } from 'axios';
-import client from './client';
-
-type userAPIFollowFunctionType = ({ userId }: userRouter.userFollowRequest) => Promise<AxiosResponse<userRouter.userFollowResponse>>
-export const userFollow: userAPIFollowFunctionType = ({ userId }: userRouter.userFollowRequest) => client.patch(`/user/${userId}/follow`);
-type myPageFunctionType = ({ userId }: userRouter.myPageRequest) => Promise<AxiosResponse<userRouter.myPageSuccessResponse>>;
-export const myPage: myPageFunctionType = ({ userId }: userRouter.myPageRequest) => client.get(`/user/${userId}`);
