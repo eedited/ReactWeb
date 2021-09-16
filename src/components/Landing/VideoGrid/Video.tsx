4572d343984e31fb7e0ea38cb22a68127952c0a5
JeonMinSu <minsu2530@u.sogang.ac.kr>
2021-09-16 15:25:52 +0900

    video 동영상 조건부 렌더링으로 변경.

--- src/components/Landing/VideoGrid/Video.tsx
+++ src/components/Landing/VideoGrid/Video.tsx
@@ -1,58 +1,56 @@
-// TODO: 투명한 사각형을 하나 올려서 거지같은 유튜브 로고도 없앨 수 있을거 같고, 클랙했을 때, 마우스 올라왔을 때 등등의 이벤트도 몰아서 처리하면 될거 같음. 그런데 이게 이미지에 이벤트 핸들링 하는거랑 다를게 있는가? 는 좀 생각해 볼 필요가 있음.
-
 import React, { forwardRef } from 'react';
 
 import ReactPlayer from 'react-player';
 import { Link } from 'react-router-dom';
-import LikeButtonCountContainer from '../../../containers/landing/LikeButtonCountContainer';
 import './Video.scss';
-import VideoDescription1 from './VideoDescription/VideoDescription1';
 
 interface props{
     onLoad: (player: ReactPlayer) => void
-    play: () => void
-    pause: () => void
-    setOpacity: () => number
+    mouseOver: boolean
+    onMouseOver: () => void
+    onMouseLeave: () => void
     videoInfo: VIDEO
     key: string|undefined
+    isLoading: boolean
 }
 const Video: React.ForwardRefExoticComponent<props & React.RefAttributes<ReactPlayer>> = forwardRef<ReactPlayer, props>(({
-    onLoad, play, pause, setOpacity, videoInfo, key,
+    onLoad, videoInfo, key, mouseOver, onMouseOver, isLoading, onMouseLeave,
 }: props, youtubeRef: React.ForwardedRef<ReactPlayer>) => (
     <div className="videoElement" key={key}>
         <Link to={`/videoInfo?videoId=${videoInfo.id}`}>
-            <div className="video">
-                <ReactPlayer
-                    className="video__player"
-                    url={videoInfo.url}
-                    ref={youtubeRef}
-                    muted
-                    width="100%"
-                    height="100%"
-                    onReady={onLoad}
-                    config={{
-                        youtube: {
-                            playerVars: {
-                                rel: 0,
-                                origin: 'http://localhost:3000',
-                            },
-                        },
-                    }}
-                    style={{
-                        opacity: setOpacity(),
-                        borderRadius: '20px',
-                    }}
-                />
+            <div
+                className="video"
+                onMouseEnter={onMouseOver}
+                onMouseLeave={onMouseLeave}
+            >
                 <img
                     className="video__img"
                     src={videoInfo.thumbnail}
                     alt="123"
-                    onMouseOver={play}
-                    onFocus={play}
-                    onMouseLeave={pause}
-                    onBlur={pause}
-                    style={{ opacity: (setOpacity() + 1) % 2 }}
+                    style={{ opacity: !isLoading && mouseOver ? 0 : 1, zIndex: 2 }}
                 />
+                {
+                    mouseOver && (
+                        <ReactPlayer
+                            className="video__player"
+                            url={videoInfo.url}
+                            ref={youtubeRef}
+                            muted
+                            width="100%"
+                            height="100%"
+                            onReady={onLoad}
+                            config={{
+                                youtube: {
+                                    playerVars: {
+                                        rel: 0,
+                                        origin: 'http://localhost:3000',
+                                    },
+                                },
+                            }}
+                            playing
+                        />
+                    )
+                }
             </div>
         </Link>
     </div>
--- src/containers/landing/VideoContainer.tsx
+++ src/containers/landing/VideoContainer.tsx
@@ -1,5 +1,3 @@
-// TODO: 투명한 사각형을 하나 올려서 거지같은 유튜브 로고도 없앨 수 있을거 같고, 클랙했을 때, 마우스 올라왔을 때 등등의 이벤트도 몰아서 처리하면 될거 같음. 그런데 이게 이미지에 이벤트 핸들링 하는거랑 다를게 있는가? 는 좀 생각해 볼 필요가 있음.
-
 import React, {
     useRef, useState, useCallback,
 } from 'react';
@@ -13,32 +11,25 @@ interface props{
 
 const VideoContainer: React.FC<props> = ({ videoInfo, key }: props) => {
     const [isLoading, setisLoading]: [boolean, React.Dispatch<React.SetStateAction<boolean>>] = useState<boolean>(true);
-    const [isPlay, setisPlay]: [boolean, React.Dispatch<React.SetStateAction<boolean>>] = useState<boolean>(false);
     const youtubeRef: React.RefObject<ReactPlayer> = useRef<ReactPlayer>(null);
-
-    const play: () => void = () => {
-        if (!isLoading) {
-            setisPlay(true);
-            youtubeRef.current?.getInternalPlayer().playVideo();
-        }
-    };
-
-    const pause: () => void = () => {
-        if (!isLoading) {
-            youtubeRef.current?.getInternalPlayer().stopVideo();
-            setisPlay(false);
-        }
-    };
+    const [mouseOver, setMouseOver]: [boolean, React.Dispatch<React.SetStateAction<boolean>>] = useState<boolean>(false);
     const onLoad: (plyaer: ReactPlayer) => void = (player: ReactPlayer) => {
         setisLoading(false);
     };
-    const setOpacity: () => number = useCallback(() => Number(!isLoading && isPlay), [isLoading, isPlay]);
+    const onMouseOver: () => void = useCallback(() => {
+        setMouseOver(true);
+    }, []);
+    const onMouseLeave: () => void = useCallback(() => {
+        setMouseOver(false);
+        setisLoading(true);
+    }, []);
     return (
         <Video
+            onMouseLeave={onMouseLeave}
+            onMouseOver={onMouseOver}
             onLoad={onLoad}
-            play={play}
-            pause={pause}
-            setOpacity={setOpacity}
+            isLoading={isLoading}
+            mouseOver={mouseOver}
             videoInfo={videoInfo}
             ref={youtubeRef}
             key={key}
