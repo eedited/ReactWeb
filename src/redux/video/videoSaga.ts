967f2d5fd150f47b8e49406a36e1d89dcff730d0
JeonMinSu <minsu2530@u.sogang.ac.kr>
2021-09-30 18:55:55 +0900

    redux video 경로 대소문자 수정.

---
+++ src/redux/video/videoSaga.ts
@@ -0,0 +1,22 @@
+import { ForkEffect, takeLatest } from 'redux-saga/effects';
+import createRequestSaga, { CreateRequestSagaReturnType } from '../createRequestSaga';
+import { videoAction } from './video';
+import * as videoAPI from '../../api/video';
+
+const videoSaga: CreateRequestSagaReturnType<VideoRouter.VideoRequest, VideoRouter.VideoSuccessResponse> = createRequestSaga('VIDEO/video', videoAPI.video);
+const videoListSaga: CreateRequestSagaReturnType<VideoRouter.VideoListRequest, VideoRouter.VideoListSuccessResponse> = createRequestSaga('VIDEO/videoList', videoAPI.videoList);
+const videoUploadSaga: CreateRequestSagaReturnType<VideoRouter.VideoUploadRequest, VideoRouter.VideoUploadSuccessResponse> = createRequestSaga('VIDEO/videoUpload', videoAPI.videoUpload);
+const videoMoreByUserSaga: CreateRequestSagaReturnType<VideoRouter.UserVideoRequest, VideoRouter.UserVideoSuccessResponse> = createRequestSaga('VIDEO/videoUserUploaded', videoAPI.videoUser);
+const videoModifySaga: CreateRequestSagaReturnType<VideoRouter.VideoModifyRequest, VideoRouter.VideoModifySuccessResponse> = createRequestSaga('VIDEO/videoModify', videoAPI.videoModify);
+const videoUserMayBeLikeSaga: CreateRequestSagaReturnType<VideoRouter.VideoListRequest, VideoRouter.VideoListSuccessResponse> = createRequestSaga('VIDEO/videoUserMayBeLike', videoAPI.videoList);
+export default function* getVideoSaga(): Generator<ForkEffect<never>, void, unknown> {
+    const {
+        video, videoList, videoUpload, videoUserUploaded, videoModify, videoUserMayBeLike,
+    }: RDXVideoModule.ActionType = videoAction;
+    yield takeLatest(video, videoSaga);
+    yield takeLatest(videoList, videoListSaga);
+    yield takeLatest(videoUpload, videoUploadSaga);
+    yield takeLatest(videoUserUploaded, videoMoreByUserSaga);
+    yield takeLatest(videoModify, videoModifySaga);
+    yield takeLatest(videoUserMayBeLike, videoUserMayBeLikeSaga);
+}
