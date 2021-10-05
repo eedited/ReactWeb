b328d92fa616d3d950760794b6628d7f9c88981f
JeonMinSu <minsu2530@u.sogang.ac.kr>
2021-10-05 16:46:42 +0900

    email 제출 시 spinner 구현

--- src/containers/accountSetting/RecieveDiscomfort.tsx
+++ src/containers/accountSetting/RecieveDiscomfort.tsx
@@ -3,6 +3,7 @@ import React, { useState, useCallback, useEffect } from 'react';
 import { AnyAction } from 'redux';
 import { discomfort, myPageModify } from '../../api/user';
 import BlueButton from '../../components/common/Button/BlueButton';
+import Spinner from '../../components/common/Spinner/Spinner';
 import useInputs, { inputType } from '../../hooks/useInputs';
 import './Setting.scss';
 
@@ -22,6 +23,7 @@ const RecieveDiscomfort: React.FC<Props> = ({ user }: Props) => {
     const [descriptionText, setDescriptionTextChange]: [string, React.Dispatch<React.SetStateAction<string>>] = useState('');
     const [submitResponse, setSubmitResponse]: [SubmitResponse, React.Dispatch<React.SetStateAction<SubmitResponse>>] = useState<SubmitResponse>({ success: null, failure: null });
     const [errMsg, setErrMsg]: [string|null, React.Dispatch<React.SetStateAction<string|null>>] = useState<string|null>(null);
+    const [loading, setLoading]: [boolean, React.Dispatch<React.SetStateAction<boolean>>] = useState<boolean>(false);
     const onDescriptionChange: (e: React.ChangeEvent<HTMLTextAreaElement>) => void = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
         setDescriptionTextChange(e.target.value);
     };
@@ -29,6 +31,7 @@ const RecieveDiscomfort: React.FC<Props> = ({ user }: Props) => {
         (async function f() {
             e.preventDefault();
             setSubmitResponse({ success: null, failure: null });
+            setLoading(true);
             setErrMsg(null);
             try {
                 const response: AxiosResponse<UserRouter.MypageModifySuccessResponse> = await discomfort({ description: descriptionText, title: inputState.title });
@@ -42,10 +45,12 @@ const RecieveDiscomfort: React.FC<Props> = ({ user }: Props) => {
                 }
                 setErrMsg('이메일을 보내는데 실패했습니다. discord로 문의해주세요');
             }
+            setLoading(false);
         }());
     }, [descriptionText, inputState.title]);
     return (
         <>
+            <Spinner loading={loading} />
             <div className="accountSetting__pageDescription">
                 <div>eedited가 어떤 점이 아쉽다. 어떤 기능이 추가되었으면 좋겠다.가 있으면 이야기해주세요!</div>
                 <div>매주 새롭고, 빠르게 발전하는 eedited를 발견할 수 있을거에요!!</div>
