b06f27b523681a9994d10b803cea80e81fe7a5cc
QuqqU <rldnd913@naver.com>
2021-10-14 09:24:14 +0900

    change env condition

--- src/api/client.ts
+++ src/api/client.ts
@@ -2,7 +2,7 @@ import axios, { AxiosInstance } from 'axios';
 
 const client: AxiosInstance = axios.create({
     withCredentials: true,
-    baseURL: process.env.NODE_ENV === 'development' ? process.env.REACT_APP_DEV_API_URL : process.env.REACT_APP_PROD_API_URL,
+    baseURL: process.env.NODE_ENV === 'production' ? process.env.REACT_APP_PROD_API_URL : process.env.REACT_APP_DEV_API_URL,
 });
 
 export default client;
